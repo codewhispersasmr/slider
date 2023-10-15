cdbbfd3f06671c22c227d4ec403c2df686596ac3
Leomar Bucud <leomar@floriusflowers.com>
2023-10-15 08:52:12 +0400

    Initial commit

---
+++ script.js
@@ -0,0 +1,33 @@
+const $left = document.querySelector('.left');
+const $right = document.querySelector('.right');
+const $slider = document.querySelector('ol');
+
+const WIDTH = document.querySelector(`.slider`).clientWidth;
+const totalWidth = $slider.querySelectorAll('li').length * WIDTH;
+
+$slider.style.width = `${totalWidth}px`;
+$slider.prepend($slider.querySelector(`li:last-child`));
+$slider.style.left = `-${WIDTH}px`;
+
+const move = (s) => {
+    const v = (WIDTH * s) - WIDTH;
+    $slider.style.transition = `all 0.5s ease`;
+    $slider.style.left = `${v}px`;
+    setTimeout(() => {
+        if( s > 0 ) {
+            $slider.prepend($slider.querySelector(`li:last-child`));
+        } else {
+            $slider.append($slider.querySelector(`li:first-child`));
+        }
+        $slider.style.transition = `none`;
+        $slider.style.left = `-${WIDTH}px`;
+    }, 500);
+};
+
+$left.onclick = () => {
+    move(1);
+};
+
+$right.onclick = () => {
+    move(-1);
+};
